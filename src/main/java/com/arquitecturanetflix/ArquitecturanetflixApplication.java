fab2895821293d1b2567e0874f9315b4ddfe99c8
Jesus Viloria <jesudvm12@gmail.com>
2022-09-17 12:58:43 -0500

    arquitecturanetflix

---
+++ src/main/java/com/arquitecturanetflix/ArquitecturanetflixApplication.java
@@ -0,0 +1,13 @@
+package com.arquitecturanetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturanetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquitecturanetflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arquitecturanetflix/ArquitecturanetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquitecturanetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturanetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
